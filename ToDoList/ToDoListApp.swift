0abe0775d187bd5ee398c931118daafbb3cc4c50
Pond <6410685033@student.tu.ac.th>
2024-03-08 09:53:49 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Thammasat Thonggamgaew on 8/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
